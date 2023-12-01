00b7277c2ae97a06baae3b53f247a6303c4d2096
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2023-12-01 10:18:51 +0700

    create sidebar

---
+++ App/src/page/admin.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "../component/sidebar";
+
+export default function Admin(){
+    return(
+        <div className="w-full min-h-screen flex justify-between">
+            <Sidebar/>
+            
+        </div>
+    )
+} 
