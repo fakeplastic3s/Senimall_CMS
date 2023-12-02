7979290b60aedebfef875c63e12bfea794064107
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2023-12-02 17:52:48 +0700

    add button logout sidebar

--- App/src/page/admin.jsx
+++ App/src/page/admin.jsx
@@ -1,12 +1,13 @@
 import Artwork from "../component/artwork";
 import Sidebar from "../component/sidebar";
 import Content from "../component/content";
+import SidebarOwner from "../component/sidebar_owner";
 
 export default function Admin() {
   return (
     <div className="w-full min-h-screen flex ">
       <Sidebar />
-      <Content />
+      
     </div>
   );
 }
