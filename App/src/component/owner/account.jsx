c32c8e9c77fc4d4b8faf4bb12081877dbbcfe7f5
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-10 16:02:34 +0700

    add component account and dashboard

---
+++ App/src/component/owner/account.jsx
@@ -0,0 +1,7 @@
+export default function Account() {
+  return (
+    <div className="min-h-screen w-[82%] px-5 md:px-10 pt-16">
+      <h1>ini halaman akun</h1>
+    </div>
+  );
+}
---
+++ App/src/component/owner/dashboard.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen w-[82%] px-5 md:px-10 pt-16">
+      <h1>ini halaman Dashboard</h1>
+    </div>
+  );
+}
