6411ce4b114e366e7fa35cfcc0d3e35912f09477
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-11 18:43:51 +0700

    Update App.jsx

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Admin from "./page/admin";
 import Login from "./page/login";
 import Owner from "./page/owner";
+import DashboardOwner from "./component/owner/dashboard";
+import UsersOwner from "./component/owner/users";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,9 +11,13 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' Component={Login}/>
-          <Route path='/admin' Component={Admin}/>
-          <Route path='/owner' Component={Owner}/>
+          <Route path="/" Component={Login} />
+          <Route path="/admin" Component={Admin} />
+          <Route path="/owner" Component={Owner}>
+            <Route path="/owner" Component={DashboardOwner} />
+            <Route path="/owner/Dashboard" Component={DashboardOwner} />
+            <Route path="/owner/Users" Component={UsersOwner} />
+          </Route>
         </Routes>
       </Router>
     </>
