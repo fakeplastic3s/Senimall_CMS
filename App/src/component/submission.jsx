557e7d9f6bf5f9db317122c1432f7dcb95aedb6f
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2023-12-12 13:41:20 +0700

    to pull 2

---
+++ App/src/component/submission.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react"
+import { useState } from "react";
+import axios from 'axios'
+
+export default function Submission() {
+
+    return (
+        <div className="min-h-screen w-full">
+            <h1 className="text-center text-3xl font-franklin">Submission</h1>
+        </div>
+    )
+}
