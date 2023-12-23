d3bffeff23532c9c5e628cbaef1167155e330fe0
Hafid Firman febrian <120733853+fakeplastic3s@users.noreply.github.com>
2023-12-23 22:12:01 +0700

    update table artwork & submission

--- App/src/component/admin/artwork_list.jsx
+++ App/src/component/admin/artwork_list.jsx
@@ -17,6 +17,7 @@ export default function ArtworkList({ sendDataAddButton }) {
   const MySwal = withReactContent(Swal);
   const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
   const [selectedArtwork, setSelectedArtwork] = useState(null); // Track the selected submission
+  let number = 1;
 
   async function getArtworkList() {
     try {
@@ -96,8 +97,10 @@ export default function ArtworkList({ sendDataAddButton }) {
       <div className="overflow-x-auto">
         <Table hoverable>
           <Table.Head className="">
+            <Table.HeadCell>#</Table.HeadCell>
             <Table.HeadCell>Title</Table.HeadCell>
             <Table.HeadCell>Artist</Table.HeadCell>
+            <Table.HeadCell>Image</Table.HeadCell>
             <Table.HeadCell>
               <span className="sr-only">Action</span>
             </Table.HeadCell>
@@ -105,35 +108,41 @@ export default function ArtworkList({ sendDataAddButton }) {
           <Table.Body className="divide-y">
             {currentArt.map((item) => (
               <Table.Row key={item.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell>{number++}</Table.Cell>
                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">{item.title}</Table.Cell>
                 <Table.Cell>{item.Artist}</Table.Cell>
-                <Table.Cell className="flex gap-3 items-center">
-                  {/* Eye Icon */}
-                  <button onClick={() => openModal(item)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                <Table.Cell>
+                  <img src={item.image} alt="" width="150px" />
+                </Table.Cell>
+                <Table.Cell>
+                  <div className="flex gap-3 items-center">
+                    {/* Eye Icon */}
+                    <button onClick={() => openModal(item)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                        />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                      </svg>
+                    </button>
+                    {/* Pencil Icon */}
+                    <Link to={`/admin/artwork/edit-artworklist/${item.id}`}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="green" className="w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                      </svg>
+                    </Link>
+
+                    {/* Trash Icon */}
+                    <svg onClick={() => handleButtonDelete(item.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="w-4 h-4 cursor-pointer">
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                       />
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                  </button>
-                  {/* Pencil Icon */}
-                  <Link to={`/admin/artwork/edit-artworklist/${item.id}`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="green" className="w-4 h-4">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                    </svg>
-                  </Link>
-
-                  {/* Trash Icon */}
-                  <svg onClick={() => handleButtonDelete(item.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="w-4 h-4 cursor-pointer">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
+                  </div>
                 </Table.Cell>
               </Table.Row>
             ))}
@@ -160,10 +169,12 @@ export default function ArtworkList({ sendDataAddButton }) {
                       <p className="font-bold">Artist</p>
                       <p className="mb-2">{selectedArtwork.Artist}</p>
                       <p className="font-bold">Price</p>
-                      <p className="mb-2">{parseInt(selectedArtwork.price, 10).toLocaleString('id-ID', {
-                                style: 'currency',
-                                currency: 'IDR',
-                              })}</p>
+                      <p className="mb-2">
+                        {parseInt(selectedArtwork.price, 10).toLocaleString("id-ID", {
+                          style: "currency",
+                          currency: "IDR",
+                        })}
+                      </p>
                       <p className="font-bold">Category</p>
                       <p className="mb-2">{selectedArtwork.category}</p>
                       <p className="font-bold">Material</p>
@@ -195,10 +206,10 @@ const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
 
   return (
     <nav>
-      <ul className="pagination flex gap-3 p-5">
+      <ul className="pagination flex gap-3 p-5 justify-end items-center">
         Page
         {pageNumbers.map((number) => (
-          <li key={number} className="page-item ">
+          <li key={number} className="page-item hover:bg-gray-200 p-1 rounded-md">
             <a onClick={() => paginate(number)} href="#" className="page-link">
               {number}
             </a>
